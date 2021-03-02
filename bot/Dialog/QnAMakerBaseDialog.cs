321725cb2accdb00a9c15d48b23e2d05c49ec230
Yeiko Santamaria <57649843+YeikoSantamaria@users.noreply.github.com>
2021-03-02 00:50:00 -0500

    Update QnAMakerBaseDialog.cs

--- bot/Dialog/QnAMakerBaseDialog.cs
+++ bot/Dialog/QnAMakerBaseDialog.cs
@@ -18,10 +18,10 @@ namespace Microsoft.BotBuilderSamples.Dialog
     public class QnAMakerBaseDialog : QnAMakerDialog
     {
         // Dialog Options parameters
-        public const string DefaultNoAnswer = "No QnAMaker answers found.";
-        public const string DefaultCardTitle = "Did you mean:";
-        public const string DefaultCardNoMatchText = "None of the above.";
-        public const string DefaultCardNoMatchResponse = "Thanks for the feedback.";
+        public const string DefaultNoAnswer = "No se han encontrado respuestas. porfavor llena el [formilario](https://ncv.microsoft.com/sCnxrwWE0Y) de ayuda";
+        public const string DefaultCardTitle = "Quizo decir:";
+        public const string DefaultCardNoMatchText = "Ninguno de los anteriores.";
+        public const string DefaultCardNoMatchResponse = "Gracias por su retroalimentación.";
         private readonly IBotServices _services;
         private readonly IConfiguration _configuration;
 
